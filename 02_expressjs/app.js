1972ba29efc2de1e6def4c7da0332e0b7484eee0
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-26 11:37:39 +0300

    Introduction to middleware scheme

--- 02_expressjs/app.js
+++ 02_expressjs/app.js
@@ -1,9 +1,15 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); // Allow the request to continue to the next middleware in line.
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);
