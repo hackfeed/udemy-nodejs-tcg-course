b9227b98ca41728a4e538f876e8d337765304115
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-26 22:52:28 +0300

    Split app into routers

--- 02_expressjs/app.js
+++ 02_expressjs/app.js
@@ -3,25 +3,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  next();
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
-  );
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
