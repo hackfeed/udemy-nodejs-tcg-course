158e8a091dd766f223fb477448375de6ea0399e1
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-27 00:02:36 +0300

    Add sendFile HTML pages

--- 02_expressjs/app.js
+++ 02_expressjs/app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -12,7 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- 02_expressjs/routes/admin.js
+++ 02_expressjs/routes/admin.js
@@ -1,11 +1,11 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
