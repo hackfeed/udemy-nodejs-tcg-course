24c924bc05eaef31df59c40f4897857412dc7fe9
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-26 21:23:25 +0300

    Complete exercise_02

--- exercise_02/app.js
+++ exercise_02/app.js
@@ -2,14 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
+// app.use((req, res, next) => {
+//   console.log("First middleware");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("Second middleware");
+//   res.send("<p>Last station bruh!</p>");
+// });
+
+app.use("/users", (req, res, next) => {
+  res.send("<h1>Users page</h1>");
 });
 
-app.use((req, res, next) => {
-  console.log("Second middleware");
-  res.send("<p>Last station bruh!</p>");
+app.use("/", (req, res, next) => {
+  res.send("<h1>Home page</h1>");
 });
 
 app.listen(3000);
