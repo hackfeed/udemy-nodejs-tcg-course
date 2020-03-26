bb940518db036df8005e2fc579e08c0e5ad39594
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-26 21:34:26 +0300

    Add body-parser

--- 02_expressjs/app.js
+++ 02_expressjs/app.js
@@ -1,19 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/", (req, res, next) => {
-  console.log("This always runs!");
   next();
 });
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>The 'Add-product' page</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
   res.send("<h1>Hello from Express!</h1>");
 });
 
