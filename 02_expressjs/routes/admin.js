383f35264f1c2f7626ea2e74dc193d5e996b024e
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-27 00:06:31 +0300

    Add helper path function

--- 02_expressjs/routes/admin.js
+++ 02_expressjs/routes/admin.js
@@ -2,10 +2,12 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
