b65a186b8515a805ed0b3bbc7e09132e098afb08
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-27 10:31:39 +0300

    Add static files serving

--- 02_expressjs/app.js
+++ 02_expressjs/app.js
@@ -9,6 +9,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
