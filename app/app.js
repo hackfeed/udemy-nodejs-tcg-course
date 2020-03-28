91d21c2a3d0bd01fad9ab63b6624caf1ba720b7b
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-28 12:57:54 +0300

    Add Pug template engine

--- app/app.js
+++ app/app.js
@@ -5,13 +5,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
