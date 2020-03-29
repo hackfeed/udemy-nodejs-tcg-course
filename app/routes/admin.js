10530ca1d67b2b2019fc734c7653536f1555775c
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-29 16:54:02 +0300

    Complete pug template

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.render("add-product", { pageTitle: "Add Product" });
+  res.render("add-product", { pageTitle: "Add Product", path: "/admin/add-product" });
 });
 
 router.post("/add-product", (req, res, next) => {
--- app/routes/shop.js
+++ app/routes/shop.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { prods: products, pageTitle: "Shop" });
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
