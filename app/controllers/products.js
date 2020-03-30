c40fd29b5c4432969d761839b46f1cab52312ee6
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-31 01:53:08 +0300

    Add file storage

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -1,14 +1,17 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", { pageTitle: "Add Product", path: "/admin/add-product" });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
+  Product.fetchAll(products => {
+    res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
+  });
 };
---
+++ app/models/product.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("../util/path");
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    const p = path.join(rootDir, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    const p = path.join(rootDir, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
+  }
+};
