cac22086ade4a6507046298d9a75e773a61fad40
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-30 12:23:58 +0300

    Add Handlebars templating

--- app/app.js
+++ app/app.js
@@ -2,10 +2,12 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine("handlebars", expressHbs());
+app.set("view engine", "handlebars");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
