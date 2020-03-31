4bdc4fb7b9b396fe4af92be6050374235535fa55
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-31 15:49:19 +0300

    Update views

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -5,7 +5,11 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+  const price = req.body.price;
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
