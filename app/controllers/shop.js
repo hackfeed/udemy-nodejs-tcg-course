1d008cad7d7a3bdffe82ee38b82abb03f0af49c4
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-31 15:53:13 +0300

    Add Orders entry

--- app/controllers/shop.js
+++ app/controllers/shop.js
@@ -20,6 +20,10 @@ exports.getCart = (req, res, next) => {
   res.render("shop/cart", { pageTitle: "Your Cart", path: "/cart" });
 };
 
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", { pageTitle: "Your Orders", path: "/orders" });
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", { pageTitle: "Checkout", path: "/checkout" });
 };
