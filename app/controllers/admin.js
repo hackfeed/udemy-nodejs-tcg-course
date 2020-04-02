e7f4e83054bf8f1f3c95d3b52868539726b0f955
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-02 11:38:59 +0300

    Add editing template

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -17,12 +17,19 @@ exports.postAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
-    res.redirect("/");
+    return res.redirect("/");
   }
-  res.render("admin/edit-product", {
-    pageTitle: "Edit Product",
-    path: "/admin/edit-product",
-    editing: editMode
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product
+    });
   });
 };
 
