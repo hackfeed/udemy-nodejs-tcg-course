c6575dc14b31439492d8bac56c0388b98e9e3793
Sergey Kononenko <hackfeed@yandex.ru>
2020-10-07 00:51:12 +0300

    Add JWT security

--- app_socialnet/backend/controllers/auth.js
+++ app_socialnet/backend/controllers/auth.js
@@ -1,8 +1,8 @@
 const { validationResult } = require("express-validator/check");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
-const user = require("../models/user");
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -23,6 +23,45 @@ exports.signup = (req, res, next) => {
       return user.save();
     })
     .then((result) => res.status(201).json({ message: "User created!", userId: result._id }))
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("A user with this email could not be found.");
+        error.statusCode = 401;
+        throw error;
+      }
+
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Wrong password.");
+        error.statusCode = 401;
+        throw error;
+      }
+
+      const token = jwt.sign(
+        { email: loadedUser.email, userId: loadedUser._id.toString() },
+        "myflexsecret",
+        { expiresIn: "1h" }
+      );
+
+      res.status(200).json({ token, userId: loadedUser._id.toString() });
+    })
     .catch((err) => {
       if (!err.statusCode) {
         err.statusCode = 500;
--- app_socialnet/backend/routes/auth.js
+++ app_socialnet/backend/routes/auth.js
@@ -26,4 +26,6 @@ router.put(
   authController.signup
 );
 
+router.post("/login", authController.login);
+
 module.exports = router;
